2104c3c81b63ceecb7bfb11dd4263043149fde1b
kyuk278937 <158030289+kyuk278937@users.noreply.github.com>
2024-05-06 11:21:25 +0300

    8

--- script.js
+++ script.js
@@ -1,57 +1,14 @@
-var imageCounter = 1;
 
-// Функція для клонування div і додавання його до div з id "stream"
-function cloneDivAndAppendToStream() {
-    // Отримуємо посилання на елемент, який ми хочемо клонувати
-    var originalDiv = document.getElementById('originalDiv');
 
-    // Перевіряємо, чи елемент існує
-    if (originalDiv) {
-        // Клонуємо оригінальний div
-        var clonedDiv = originalDiv.cloneNode(true);
-        
-        // Отримуємо посилання на додатковий div з класом "images" у клонованому div
-        var imagesDiv = clonedDiv.querySelector('.images');
-        
-        // Перевіряємо, чи елемент imagesDiv існує
-        if (imagesDiv) {
-            // Отримуємо всі зображення в imagesDiv
-            var images = imagesDiv.querySelectorAll('img');
+function createPost(number) {
+    var streemDiv = document.getElementById("stream")
 
-            // Отримуємо лічильник зображень, щоб змінювати шляхи відповідно
-
-            // Ітеруємося по кожному зображенню і змінюємо його src на нове зображення
-            images.forEach(function(image) {
-                // Припустимо, що ваші зображення мають імена image1.jpg, image2.jpg, і т. д.
-                var newImageSrc = 'images/image' + imageCounter + '.jpg';
-                image.setAttribute('src', newImageSrc);
-
-                // Збільшуємо лічильник зображень для наступного зображення
-                imageCounter++;
-            });
-            
-            // Отримуємо посилання на div з id "stream"
-            var streamDiv = document.getElementById('stream');
-            
-            // Перевіряємо, чи елемент streamDiv існує
-            if (streamDiv) {
-                // Додаємо клонований div до div з id "stream"
-                streamDiv.appendChild(clonedDiv);
-            } else {
-                console.error("Елемент з id 'stream' не знайдено.");
-            }
-        } else {
-            console.error("Елемент з класом 'images' в клонованому div не знайдено.");
-        }
-    } else {
-        console.error("Елемент з id 'originalDiv' не знайдено.");
-    }
+    streemDiv.innerHTML += `<div class="post"><def class="images"><img src="images/image${number}.jpg" alt="виконавець роботи"></def><def class="description"><div class="screen_shots"><img src="images/screenShot${number}.png" alt="скріншот гри"></div><div class="info"><div><h2>Заголовок</h2></div><div><p>Опис</p></div><div><a href="#"  class="black-button">сторінка гри</a><br></div></div></def></div>`
 }
 
-// Чекаємо, коли DOM повністю завантажено
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Клонуємо div 5 разів та додаємо його до div з id "stream"
-    for (var i = 0; i < 4; i++) {
-        cloneDivAndAppendToStream();
+    for (var number = 0; number < 15; number++) {
+        createPost(number);
     }
 });
